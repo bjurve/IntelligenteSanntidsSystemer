b49fe44fe4fc9b2d9618f643c1063b52628656b7
bjurve <sondre00bjerve@gmail.com>
2025-10-17 15:00:17 +0200

    kjøøør

---
+++ Code/src/Drive_Task.ads
@@ -0,0 +1,8 @@
+with MicroBit.MotorDriver; use MicroBit.MotorDriver;
+with DFR0548;
+with Ada.Real_Time; use Ada.Real_Time;
+
+package Drive_Task is
+   task Motor_Control;
+end Drive_Task;
+
---
+++ Code/src/ultrasonic_detect.ads
@@ -0,0 +1,4 @@
+package Ultrasonic_Detect is
+   function Min_Distance return Integer;   -- cm
+   function Obstacle_Near return Boolean;  -- True hvis ≤ 3 cm
+end Ultrasonic_Detect;
